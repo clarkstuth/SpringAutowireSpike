34c3c7c240af894f43ae7661eb466002d7c8a575
Clark Stuth <clarkstuth@gmail.com>
2015-05-08 23:38:17 +0000

    formatting

--- src/main/java/com/clarkstuth/SpringSpike/GenericDaoImpl.java
+++ src/main/java/com/clarkstuth/SpringSpike/GenericDaoImpl.java
@@ -9,11 +9,6 @@ public class GenericDaoImpl implements GenericDao {
 	@Value("${app.myProperty:bar}")
 	private String myProperty;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.clarkstuth.SpringSpike.IGenericDao#saveData(java.lang.Object)
-	 */
 	public void saveData(String something) {
 		System.out.println(String.format("I saved: %s with property %s", something, myProperty));
 	}
